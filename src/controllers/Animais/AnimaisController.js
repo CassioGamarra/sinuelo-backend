38f6b2a15ceec709993e890ff7f0315e7ede166e
cassiogamarra <cassiogamarra@outlook.com>
2021-11-14 01:50:46 -0300

    Finalização do controller de animais

--- src/controllers/Animais/AnimaisController.js
+++ src/controllers/Animais/AnimaisController.js
@@ -58,20 +58,32 @@ module.exports = {
     
     if (isAdmin) {
       const schema = req.schema;
-      const idFazenda = req.params.id; 
+      const idAnimal = req.params.id; 
 
       const sqlSelect = {
         text: ` 
           SELECT 
-            ID_FAZENDA AS ID,
-            NOME,
-            CEP,
-            CIDADE,
-            ESTADO
-          FROM ${schema}.FAZENDA
-          WHERE ID_FAZENDA = $1
+            A.ID_ANIMAL AS ID,   
+            A.ID_FAZENDA, 
+            F.NOME AS FAZENDA,
+            A.ID_PIQUETE,
+            P.NOME AS PIQUETE, 
+            A.ID_RACA, 
+            R.NOME AS RACA,
+            A.NOME, 
+            A.SEXO, 
+            TO_CHAR(A.DATA_NASCIMENTO, 'DD/MM/YYYY') AS DT_NASCIMENTO, 
+            A.NOME_PAI, 
+            A.NOME_MAE, 
+            A.PESO, 
+            A.PELAGEM
+          FROM ${schema}.ANIMAIS A
+            INNER JOIN ${schema}.FAZENDAS F ON A.ID_FAZENDA = F.ID_FAZENDA
+            INNER JOIN ${schema}.RACAS R ON A.ID_RACA = R.ID_RACA
+            LEFT JOIN ${schema}.PIQUETES P ON A.ID_PIQUETE  = P.ID_PIQUETE
+          WHERE ID_ANIMAL = $1
         `,
-        values:[idFazenda]
+        values:[idAnimal]
       }
 
       pool.connect((err, client, done) => {
@@ -138,11 +150,14 @@ module.exports = {
     if (isAdmin) {
       const schema = req.schema;
       const dados = req.body;
-      const idFazenda = req.params.id;
+      const idAnimal = req.params.id; 
+
+      console.log(dados.ID_PIQUETE)
 
       const sqlUpdate = {
-        text: `UPDATE ${schema}.FAZENDA SET NOME = $1, CEP = $2, CIDADE = $3, ESTADO = $4 WHERE ID_FAZENDA = $5`,
-        values: [dados.NOME, dados.CEP, dados.CIDADE, dados.ESTADO, idFazenda] 
+        text: `UPDATE ${schema}.ANIMAIS SET ID_FAZENDA = $1, ID_PIQUETE = $2, ID_RACA = $3, NOME = $4, SEXO = $5, DATA_NASCIMENTO = $6, 
+        NOME_PAI = $7, NOME_MAE = $8, PESO = $9, PELAGEM = $10 WHERE ID_ANIMAL = $11`,
+        values: [dados.ID_FAZENDA, dados.ID_PIQUETE, dados.ID_RACA, dados.NOME, dados.SEXO, dados.DATA_NASCIMENTO, dados.NOME_PAI, dados.NOME_MAE, dados.PESO, dados.PELAGEM, idAnimal] 
       }
 
       pool.connect((err, client, done) => {
@@ -153,9 +168,9 @@ module.exports = {
             done(); 
             res.json({
               statusCode: 200,
-              title: "Editar Fazenda",
+              title: "Editar Animal",
               cadastrado: true,
-              message: "Fazenda atualizada com sucesso!",
+              message: "Animal atualizado com sucesso!",
             });
           }
         }); 
@@ -175,11 +190,11 @@ module.exports = {
     
     if (isAdmin) {
       const schema = req.schema; 
-      const idFazenda = req.params.id;
+      const idAnimal = req.params.id;
 
       const sqlUpdate = {
-        text: `DELETE FROM ${schema}.FAZENDA WHERE ID_FAZENDA = $1`,
-        values: [idFazenda] 
+        text: `DELETE FROM ${schema}.ANIMAIS WHERE ID_ANIMAL = $1`,
+        values: [idAnimal] 
       }
 
       pool.connect((err, client, done) => {
@@ -190,9 +205,9 @@ module.exports = {
             done(); 
             res.json({
               statusCode: 200,
-              title: "Excluir Fazenda",
+              title: "Excluir Animal",
               deletado: true,
-              message: "Fazenda excluída com sucesso!",
+              message: "Animal excluído com sucesso!",
             });
           }
         }); 
